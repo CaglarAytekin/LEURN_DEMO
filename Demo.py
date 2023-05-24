3d06508d3bcc5dade1cfb0c88a4dc3d6f17dfe70
Caglar <cagosmail@gmail.com>
2023-05-24 07:39:21 +0300

    minor updates

--- Demo.py
+++ Demo.py
@@ -9,9 +9,9 @@ learning_rate=5e-3 #initial learning rate
 cycle_no=2 # how many cycles to train (in each cycle, learning rate is reduced)
 lr_reduction=5 #reduction rate of learning rate per cycle
 epoch_no=250 # how many epochs to train per cycle
-depth=4  #model depth
-quant_no=10  #tanh quantization number
-drop_rate=0.1 #dropout rate
+depth=10 #model depth
+quant_no=5  #tanh quantization number
+drop_rate=0.1 #dropout rate : non-zero dropout is heavily recommended for more meaningful explanations
 tasktype=-1 # 0: binary classification, 1: multi-class classification, -1: regression
 dataset_name='housing' #name of the dataset - regression case
 
@@ -22,7 +22,7 @@ X_train,X_val, X_test, y_train, y_val, y_test, y_max, X_names, X_mean=read_parti
 model,model_analyse=train_model(X_train,y_train,X_val,y_val,depth,quant_no,drop_rate,tasktype,dataset_name,batch_size,learning_rate,cycle_no,epoch_no,lr_reduction,verbose=2)
 
 # #EVALUATE A SAMPLE FROM TEST DATA
-sample_indice=4
+sample_indice=1
 test_sample=X_test[sample_indice:sample_indice+1]
 
 # #RETURN EXPLANATIONS
