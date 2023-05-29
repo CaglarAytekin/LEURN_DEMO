400628f14cbe072768c950536b4c0eed76269b1a
CaglarAytekin <cagosmail@gmail.com>
2023-05-30 01:15:31 +0300

    Update example.py, changed epoch no to 100

--- example.py
+++ example.py
@@ -14,7 +14,7 @@ X_train, X_val, X_test, y_train, y_val, y_test, y_max, X_names, X_mean, X_std = 
     data, target_name="median_house_value", task_type="reg"
 )
 
-model: LEURN = train_model(X_train, y_train, X_val, y_val, task_type="reg", output_path=output_path, epoch_no=1)
+model: LEURN = train_model(X_train, y_train, X_val, y_val, task_type="reg", output_path=output_path, epoch_no=100)
 test_sample = X_test[0:1, :]
 explain = model.explain(test_sample, feat_names=X_names, y_max=y_max)
 plot_explaination(explain, os.path.join(output_path, "explain.png"))
@@ -30,7 +30,7 @@ data = load_data("iris")
 X_train, X_val, X_test, y_train, y_val, y_test, y_max, X_names, X_mean, X_std = read_partition_process_data(
     data, target_name="target", task_type="cls"
 )
-model: LEURN = train_model(X_train, y_train, X_val, y_val, task_type="cls", output_path=output_path, epoch_no=1)
+model: LEURN = train_model(X_train, y_train, X_val, y_val, task_type="cls", output_path=output_path, epoch_no=100)
 test_sample = X_test[0:1, :]
 explain = model.explain(test_sample, feat_names=X_names, y_max=y_max)
 plot_explaination(explain, os.path.join(output_path, "explain.png"))
